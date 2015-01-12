084c599322c0fbc1b5c3e0fd5412fef04fa3f8cd
Robert Bridge <robert@robbieab.com>
2015-01-12 22:31:33 +0000

    Fixing #!, moving current content to a function

--- import.sh
+++ import.sh
@@ -1,38 +1,41 @@
-#!/bin/bash
+#!/bin/sh
 
-archive="/home/robert/Pictures/Photos"
+archive(){
+  local arcdir="/home/robert/Pictures/Photos"
 
-if [ ! -d "$archive" ] ; then
-  echo "Archive directory does not exist."
-  exit 1
-fi
+  if [ ! -d "$arcdir" ] ; then
+    echo "Archive directory does not exist."
+    return 1
+  fi
 
-if [ "$#" -ne 1 ] ; then
-  echo "No argument provided, or too many arguments provided."
-  exit 1
-fi
+  if [ "$#" -ne 1 ] ; then
+    echo "No argument provided, or too many arguments provided."
+    return 1
+  fi
 
-if [ ! -f "$1" ] ; then
-  echo "File not found. Can't find file: $1."
-  exit 2
-fi
+  if [ ! -f "$1" ] ; then
+    echo "File not found. Can't find file: $1."
+    return 2
+  fi
 
-filedate=$( date -r "$1" +%Y-%m-%d )
+  local filedate=$( date -r "$1" +%Y-%m-%d )
+  local destdir="$arcdir/$filedate"
 
-destdir="$archive/$filedate"
+  if [ ! -e "$destdir" ] ; then
+    mkdir -p "$destdir"
+  fi
 
-if [ ! -e "$destdir" ] ; then
-  mkdir -p "$destdir"
-fi
+  if [ ! -d "$destdir" ] ; then
+    echo "The destination exists but is not a directory"
+    return 3
+  fi
 
-if [ ! -d "$destdir" ] ; then
-  echo "The destination exists but is not a directory"
-  exit 3
-fi
+  if [ -a "$destdir/$1" ] ; then 
+    echo "$1 already exists in archive"
+    return 1
+  fi
 
-if [ -a "$destdir/$1" ] ; then 
-  echo "$1 already exists in archive"
-  exit 1
-fi
+  cp "$1" "$destdir/"
+}
 
-cp "$1" "$destdir/"
+archive $1
